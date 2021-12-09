23fec552d3d9a261f7c620245fbcaa660d6654fa
Gean Reyes <sagitario1990_11@hotmail.com>
2021-12-09 11:51:38 -0500

    Se cambio a un sitema de modulos

---
+++ src/App.js
@@ -0,0 +1,10 @@
+export default function App(){
+    console.log("Ejecutando la app");
+    const d = document;
+    const $root = d.getElementById("root");
+    $root.innerHTML = `
+        <h1>Bienvenidos!!, Yo soy Gean Carlo Reyes Alayo</h1>
+        <p>Esta es mi primera version de pagina web personal.</p>
+    `; 
+
+}
