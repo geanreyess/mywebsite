3be56e9e4aee5e27b500987d9f5251a797c7a39e
Gean Reyes <sagitario1990_11@hotmail.com>
2021-12-09 12:25:17 -0500

    Se hicieron varias madificaciones listas para trabajar en el header de la pagina

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
+import {Header} from "./components/Header.js";
+
 export default function App(){
     console.log("Ejecutando la app");
     const d = document;
     const $root = d.getElementById("root");
-    $root.innerHTML = `
-        <h1>Bienvenidos!!, Yo soy Gean Carlo Reyes Alayo</h1>
-        <p>Esta es mi primera version de pagina web personal.</p>
-    `; 
-
+    $root.appendChild(Header());
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+export function Header(){
+    const $header = document.createElement("header");
+    $header.classList.add("header");
+    $header.innerHTML = `<h1>Aqui se diseñara el Header de esta pagina</h1>`;
+
+
+
+
+    return $header;
+}
