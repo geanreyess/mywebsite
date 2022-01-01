d18757a3c94eedc4e4328dcdf09f8071788818a5
Gean Reyes <sagitario1990_11@hotmail.com>
2022-01-01 17:05:25 -0500

    Menu desplegable terminado al 100%.

--- src/components/MenuBurger.js
+++ src/components/MenuBurger.js
@@ -1,16 +1,22 @@
 export default function MenuBurger(panelBtn, panel, menuLink) {
     const d = document;
+    let toggle;
     d.addEventListener('click', (e) => {
-        console.log("Se hizo click")
+        
         if(e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)){
-            d.querySelector(panel).classList.toggle("is-active")
-            d.querySelector(panelBtn).classList.toggle("is-active")
-            console.log("Se activo")
+            toggle = d.querySelector(panel).classList.toggle("is-active")
+            if(!toggle){
+                d.querySelector(panel).classList.add("not-active")
+            }else {
+                d.querySelector(panel).classList.remove("not-active")
+
+            }
+            
         }
 
         if(e.target.matches(menuLink)){
             d.querySelector(panel).classList.remove("is-active")
-            d.querySelector(panelBtn).classList.remove("is-active")
+            // d.querySelector(panelBtn).classList.remove("is-active")
             console.log("Se desactivo")
         }
 
