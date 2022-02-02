ac05f76a929421f75bbe1e048b22500eadf39a9c
Gean Reyes <sagitario1990_11@hotmail.com>
2022-02-02 13:37:39 -0500

    Se avanzo las seccion del portafolio al 50%

--- src/components/Portafolio.js
+++ src/components/Portafolio.js
@@ -1,9 +1,49 @@
 export default function Portafolio(){
     const $portafolio = document.createElement("section");
     $portafolio.classList.add("sections");
+    $portafolio.classList.add("portfolios-container");
     $portafolio.id = "portafolio";
     $portafolio.innerHTML = `
-        <h2>Seccion del Portafolio</h2>
+        <h2>Mi Portafolio</h2>
+        <article class="portfolio">
+            <div class="img-proyect"></div>
+            <div class="portfolio-info">
+                <p class="pulso"><a href="https://geanreyess.github.io" target="_blank">Visita el Proyecto</a></p>
+                <div class="reference-container">
+                    <img src="src/assets/avatares/avatar-1.jpg" alt="Avatar del CEO">
+                    <div class="comments">
+                        <p>"Breve agradecieminmto del CEO de la Empresa"</p>
+                        <p><i>Nombre del CEO</i></p>
+                    </div>
+                </div>
+            </div>
+        </article>
+        <article class="portfolio">
+            <div class="img-proyect"></div>
+            <div class="portfolio-info">
+                <p class="pulso"><a href="https://geanreyess.github.io" target="_blank">Visita el Proyecto</a></p>
+                <div class="reference-container">
+                    <img src="src/assets/avatares/avatar-1.jpg" alt="Avatar del CEO">
+                    <div class="comments">
+                        <p>"Breve agradecieminmto del CEO de la Empresa"</p>
+                        <p><i>Nombre del CEO</i></p>
+                    </div>
+                </div>
+            </div>
+        </article>
+        <article class="portfolio">
+            <div class="img-proyect"></div>
+            <div class="portfolio-info">
+                <p class="pulso"><a href="https://geanreyess.github.io" target="_blank">Visita el Proyecto</a></p>
+                <div class="reference-container">
+                    <img src="src/assets/avatares/avatar-1.jpg" alt="Avatar del CEO">
+                    <div class="comments">
+                        <p>"Breve agradecieminmto del CEO de la Empresa"</p>
+                        <p><i>Nombre del CEO</i></p>
+                    </div>
+                </div>
+            </div>
+        </article>
     `;
     
     return $portafolio;
