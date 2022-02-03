0275390c95a7ac07015e5f10b0da77a25b3feeb9
Gean Reyes <sagitario1990_11@hotmail.com>
2022-02-02 22:47:21 -0500

    Se modifico el tamaño de el avatar

--- src/components/Portafolio.js
+++ src/components/Portafolio.js
@@ -18,32 +18,7 @@ export default function Portafolio(){
                 </div>
             </div>
         </article>
-        <article class="portfolio">
-            <div class="img-proyect"></div>
-            <div class="portfolio-info">
-                <p class="pulso"><a href="https://geanreyess.github.io" target="_blank">Visita el Proyecto</a></p>
-                <div class="reference-container">
-                    <img src="src/assets/avatares/avatar-1.jpg" alt="Avatar del CEO">
-                    <div class="comments">
-                        <p>"Breve agradecieminmto del CEO de la Empresa"</p>
-                        <p><i>Nombre del CEO</i></p>
-                    </div>
-                </div>
-            </div>
-        </article>
-        <article class="portfolio">
-            <div class="img-proyect"></div>
-            <div class="portfolio-info">
-                <p class="pulso"><a href="https://geanreyess.github.io" target="_blank">Visita el Proyecto</a></p>
-                <div class="reference-container">
-                    <img src="src/assets/avatares/avatar-1.jpg" alt="Avatar del CEO">
-                    <div class="comments">
-                        <p>"Breve agradecieminmto del CEO de la Empresa"</p>
-                        <p><i>Nombre del CEO</i></p>
-                    </div>
-                </div>
-            </div>
-        </article>
+        
     `;
     
     return $portafolio;
