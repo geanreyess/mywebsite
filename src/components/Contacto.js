5d4e87f3bebadb8e9473bdb1c038ed8dfdf6dd33
Gean Reyes <sagitario1990_11@hotmail.com>
2022-02-04 17:58:09 -0500

    Se avanzo los estilos del formulario de contacto

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -1,9 +1,18 @@
 export default function Contacto(){
     const $contacto = document.createElement("section");
     $contacto.classList.add("sections");
+    $contacto.classList.add("contact-container");
     $contacto.id = "contacto";
     $contacto.innerHTML = `
-        <h2>contactame</h2>
+        <h2>Contactame</h2>
+        <p>"Si estas interezado en hacer negocios conmigo dejame tu correo y un mensaje explicandome en que consiste el proyecto."</p>
+        <form action="">
+            <label for="email">Correo</label>
+            <input type="text" name="email" id="email" required placeholder="Escribe tu email">
+            <label for="comment">Mensaje</label>
+            <textarea name="comment" id="comment" cols="30" rows="10" required placeholder="Escribe tu mensaje aqui" resize="none"></textarea>
+            <input type="submit" value="Enviar">        
+        </form>
     `;
     
     return $contacto;
