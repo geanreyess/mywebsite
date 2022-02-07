fd0c25b7a03e4e877bf0f1d5aa6a85c2c45e6af5
Gean Reyes <sagitario1990_11@hotmail.com>
2022-02-07 13:21:40 -0500

    Se termino la validacion de formularios

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -6,9 +6,9 @@ export default function Contacto(){
     $contacto.innerHTML = `
         <h2>Contactame</h2>
         <p>"Si estas interezado en hacer negocios conmigo dejame tu correo y un mensaje explicandome en que consiste el proyecto."</p>
-        <form action="">
+        <form action="https://formsubmit.co/sagitario1990_11@hotmail.com" method="POST">
             <label for="email">Correo</label>
-            <input type="text" name="email" id="email" required placeholder="Escribe tu email">
+            <input type="email" name="email" id="email" required placeholder="Escribe tu email" pattern="^[a-zA-Z0-9.!#$%&'*+\/=?^_{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$" title="Formato de correo invalido">
             <label for="comment">Mensaje</label>
             <textarea name="comment" id="comment" cols="30" rows="10" required placeholder="Escribe tu mensaje aqui" resize="none"></textarea>
             <input type="submit" value="Enviar">        
