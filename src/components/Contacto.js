23235c6fe19dd9849e823f8f7140573e0c910565
Gean Reyes <sagitario1990_11@hotmail.com>
2022-02-07 15:57:14 -0500

    Se cambio el correo por una cadena de texto de validacion

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -6,7 +6,7 @@ export default function Contacto(){
     $contacto.innerHTML = `
         <h2>Contactame</h2>
         <p>"Si estas interezado en hacer negocios conmigo dejame tu correo y un mensaje explicandome en que consiste el proyecto."</p>
-        <form action="https://formsubmit.co/sagitario1990_11@hotmail.com" method="POST">
+        <form action="https://formsubmit.co/3a2c46427c3264386fcd28579a80ba1b" method="POST">
             <label for="email">Correo</label>
             <input type="email" name="email" id="email" required placeholder="Escribe tu email" pattern="^[a-zA-Z0-9.!#$%&'*+\/=?^_{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$" title="Formato de correo invalido">
             <label for="comment">Mensaje</label>
