25b6ebca48de69e51c0c85ec2eef970678f38af6
Gean Reyes <sagitario1990_11@hotmail.com>
2022-02-13 11:29:45 -0500

    Se dio unos retoques al panel y se hizo una slider horizontal para el portafolio

--- src/components/Portafolio.js
+++ src/components/Portafolio.js
@@ -5,19 +5,53 @@ export default function Portafolio(){
     $portafolio.id = "portafolio";
     $portafolio.innerHTML = `
         <h2>Mi Portafolio</h2>
-        <article class="portfolio">
-            <div class="img-proyect"></div>
-            <div class="portfolio-info">
-                <p class="pulso"><a href="https://geanreyess.github.io" target="_blank">Visita el Proyecto</a></p>
-                <div class="reference-container">
-                    <img src="src/assets/avatares/avatar-1.jpg" alt="Avatar del CEO">
-                    <div class="comments">
-                        <p>"Breve agradecieminmto del CEO de la Empresa"</p>
-                        <p><i>Nombre del CEO</i></p>
+        <div class="container-scroll-portfolio">
+            <article class="portfolio">
+                <div class="img-proyect">
+                    <img src="src/assets/portafolio/web-1.jpg">
+                </div>
+                <div class="portfolio-info">
+                    <p class="pulso"><a href="https://geanreyess.github.io" target="_blank">Visita el Proyecto</a></p>
+                    <div class="reference-container">
+                        <img src="src/assets/avatares/avatar-1.PNG" alt="Avatar del CEO">
+                        <div class="comments">
+                            <p>"Breve agradecieminmto del CEO de la Empresa"</p>
+                            <p><i>Nombre del CEO</i></p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </article>
+            </article>
+            <article class="portfolio">
+                <div class="img-proyect">
+                    <img src="src/assets/portafolio/web-2.jpg">
+                </div>
+                <div class="portfolio-info">
+                    <p class="pulso"><a href="https://geanreyess.github.io" target="_blank">Visita el Proyecto</a></p>
+                    <div class="reference-container">
+                        <img src="src/assets/avatares/avatar-2.PNG" alt="Avatar del CEO">
+                        <div class="comments">
+                            <p>"Breve agradecieminmto del CEO de la Empresa"</p>
+                            <p><i>Nombre del CEO</i></p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article class="portfolio">
+                <div class="img-proyect">
+                    <img src="src/assets/portafolio/web-3.jpg">
+                </div>
+                <div class="portfolio-info">
+                    <p class="pulso"><a href="https://geanreyess.github.io" target="_blank">Visita el Proyecto</a></p>
+                    <div class="reference-container">
+                        <img src="src/assets/avatares/avatar-3.PNG" alt="Avatar del CEO">
+                        <div class="comments">
+                            <p>"Breve agradecieminmto del CEO de la Empresa"</p>
+                            <p><i>Nombre del CEO</i></p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </div>
         
     `;
     
