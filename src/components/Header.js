1bba1a6e68bfad39deb37e2cda792b9a653f6557
Gean Reyes <sagitario1990_11@hotmail.com>
2022-02-23 22:16:35 -0500

    Se modifico el header y se cambio el logo por solo el nombre

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,11 @@ export function Header(){
     const $header = document.createElement("header");
     $header.classList.add("header");
     $header.innerHTML = `
-        <div class="logo">
-            <img src="src/assets/logo.png" alt="Logo">
-            <p>Gean Reyes</p>
-        </div>
+        <span>Gean Reyes</span>
         <img class="burger-btn" src="src/assets/menu-burger.svg" alt="Burger">
     `;
 
     
-
-
-
 
     return $header;
 }
