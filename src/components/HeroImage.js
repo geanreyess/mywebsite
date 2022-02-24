d68894a1ec272362c413e6159e20a57bfb9d37ec
Gean Reyes <sagitario1990_11@hotmail.com>
2022-02-23 23:25:22 -0500

    Se modifico un poco la heroimagen

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -3,12 +3,11 @@ export default function HeroImage(){
     const $heroImage = document.createElement("section");
     $heroImage.innerHTML = `
         <article class="heroImage">
-            <img class="photo-perfil" src="./src/assets/img/foto-perfil.jpg" alt="Foto-GeanReyes">
             <div class="data">
-                <p class="title">Hola, soy Gean Reyes</p>
-                <p class="title">Hago paginas Web</p>
-                <p class="comment">Soy un desarrollador de software orientado a paginas web del lado del cliente.</p>
+                <p class="title">Hola, soy Gean Reyes...</p>
+                <p class="title">y desarrollo para la web</p>
             </div>
+            <img class="photo-perfil" src="./src/assets/img/foto-perfil.jpg" alt="Foto-GeanReyes">
         </article>
     `;
     
