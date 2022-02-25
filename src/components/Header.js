afb4e608046e8989567defbd24139a2b121c14cd
Gean Reyes <sagitario1990_11@hotmail.com>
2022-02-24 22:42:53 -0500

    Se termino de agregar los iconos de las redes sociales al header y se agrego iconos en el panel

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,12 @@ export function Header(){
     $header.classList.add("header");
     $header.innerHTML = `
         <span>Gean Reyes</span>
+        <div class="social-media-container">
+            <a href="https://www.facebook.com/geancarlo.reyesalayo.3/" target="_blank"><img src="src/assets/svg/facebook-brands.svg" alt="facebook"></a>
+            <a href=""><img src="src/assets/svg/instagram-brands.svg" alt="Instagram"></a>
+            <a href=""><img src="src/assets/svg/linkedin-brands.svg" alt="linkedin"></a>
+            <a href=""><img src="src/assets/svg/twitter-brands.svg" alt="twitter"></a>
+        </div>
         <img class="burger-btn" src="src/assets/menu-burger.svg" alt="Burger">
     `;
 
--- src/components/Panel.js
+++ src/components/Panel.js
@@ -4,10 +4,10 @@ export default function Panel(){
     $panel.classList.add("panel");
     $panel.innerHTML = `
         <nav class="menu">
-            <a href="index.html#portafolio" data-scroll-spy>Portafolio</a>
-            <a href="index.html#skillset" data-scroll-spy>Habilidades</a>
-            <a href="index.html#tecnologias" data-scroll-spy>Tecnologías</a>
-            <a href="index.html#contacto" data-scroll-spy>Contacto</a>
+            <a href="index.html#portafolio" data-scroll-spy><span><img src="src/assets/svg/suitcase-solid.svg" alt="suitcase"></span>Portafolio</a>
+            <a href="index.html#skillset" data-scroll-spy><span><img src="src/assets/svg/award-solid.svg" alt="Award"></span>Habilidades</a>
+            <a href="index.html#tecnologias" data-scroll-spy><span><img src="src/assets/svg/satellite-dish-solid.svg" alt="satellite"></span>Tecnologías</a>
+            <a href="index.html#contacto" data-scroll-spy><span><img src="src/assets/svg/paper-plane-solid.svg" alt="paper-plane"></span>Contacto</a>
         </nav>
         <p><span class="cp">&copy;</span> Copyright 2022</p>
     `;
