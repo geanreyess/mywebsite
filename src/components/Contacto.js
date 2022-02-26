45b675038c995f957008e4bf9d3d70dc3da981e8
Gean Reyes <sagitario1990_11@hotmail.com>
2022-02-26 12:15:06 -0500

    Se les quito los label a l formularios

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -7,9 +7,7 @@ export default function Contacto(){
         <h2>Contactame</h2>
         <p>"Si estas interesado en trabajar conmigo dejame tu correo y un mensaje explicandome en que consiste el proyecto."</p>
         <form action="https://formsubmit.co/3a2c46427c3264386fcd28579a80ba1b" method="POST">
-            <label for="email">Correo</label>
             <input type="email" name="email" id="email" required placeholder="Escribe tu email" pattern="^[a-zA-Z0-9.!#$%&'*+\/=?^_{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$" title="Formato de correo invalido">
-            <label for="comment">Mensaje</label>
             <textarea name="comment" id="comment" cols="30" rows="10" required placeholder="Escribe tu mensaje aqui" resize="none"></textarea>
             <input type="submit" value="Enviar">        
         </form>
