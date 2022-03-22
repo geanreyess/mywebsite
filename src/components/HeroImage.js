bcb3b97010557bcfe82d42dad63e50a0750771f0
Gean Reyes <sagitario1990_11@hotmail.com>
2022-03-22 12:33:36 -0500

    Se cambio el titulo de la hero imagen y se le dio un efecto de maquina de escribir

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -5,7 +5,7 @@ export default function HeroImage(){
         <article class="hero-image" style="--hero-image:url('../img/hero-image-2.jpg');--hero-attachment: fixed;">
             <aside class="hero-image-opacity" style="--hero-opacity-color: var(--black-alpha-color)">
                 <div class="hero-image-content">
-                    <h2 class="hero-image-title" style="--hero-text-color: var(--white-color)">Hola, soy Gean Reyes...<br>y desarrollo para la web</h2>
+                    <h2 class="hero-image-title" style="--hero-text-color: var(--white-color)"><span>¡Hola!, Soy Gean Reyes</span></h2>
                     <img class="photo-perfil" src="./src/assets/img/foto-perfil.jpg" alt="Foto-GeanReyes">
                     <a href="index.html#contacto" class="btn btn-hero-image">CONTACTAME</a>
                 </div>
